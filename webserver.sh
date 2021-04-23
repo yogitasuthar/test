ec4c352ba48d25a5fb2f868f3260314604426af5
root <yogitasuthar98@gmail.com>
2021-04-23 10:27:56 +0000

    This is for jenkins

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install httpd
+yum install -y httpd
 echo "<h1>This webpage has been built with the help of Jenkins server</h1>" > /var/www/html/index.html
 systemctl start httpd
 systemctl status httpd
